9d1633bbddd35f76a8e5493f0d52ff93ad13331b
MatteoVollero <m.vollero120796@gmail.com>
2020-09-01 17:11:34 +0200

    campare/scompare

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-$("hamburger-menu").removeClass();
+var hamburger = $("i");
+var a = $("a");
+var hide = false;
+
+a.css("width",69);
+
+
+a.click(
+  function(){
+
+    if(hide == false){
+      hamburger.fadeOut("slow");
+      hide = true;
+    }else {
+      hamburger.fadeIn("slow");
+      hide = false;
+    }
+  }
+);
